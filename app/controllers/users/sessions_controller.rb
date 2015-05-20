a6a214199bee7b6353d0df2e0a8144e44beb26fb
Victoria Li <wingman.li@hotmail.com>
2015-05-20 15:53:10 +0800

    Fix login/ logout devise front end

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,25 +1,29 @@
 class Users::SessionsController < Devise::SessionsController
-# before_filter :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  skip_before_filter :verify_signed_out_user, only: :destroy
+  
+  def create
+    resource = warden.authenticate!(:scope => resource_name, :recall => "users/sessions#failure")
+    return sign_in_and_redirect(resource_name, resource)
+  end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def sign_in_and_redirect(resource_or_scope, resource=nil)
+    scope = Devise::Mapping.find_scope!(resource_or_scope)
+    resource ||= resource_or_scope
+    sign_in(scope, resource) unless warden.user(scope) == resource
+    return render json: {:success => true, :message => "success"}, status: 200
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def failure
+    return render json: {:success => false, :message => "failed"}, status: 400
+  end
 
-  # protected
+  def destroy
+    signed_out = sign_out(resource_name)
+    render json: {:success => true, :message => "logged out"}
+  end
 
-  # You can put the params you want to permit in the empty array.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.for(:sign_in) << :attribute
-  # end
+  def authenticated
+    render json: current_user
+  end
 end
