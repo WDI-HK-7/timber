e49e8ef9cc3e41fa37cb5f430cda48050076cacd
Wilson Cheng <wilzon@gmail.com>
2015-05-21 21:59:29 +0800

    added likes relationship

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :user
 
+  has_many :likes
+  has_many :like_users, through: :likes, source: :user
+
   # #paperclip syntax. > is more syntax
 
   has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>"}
